2dd29410ffaf70e8e6e6c62bc76d96d40bebe3e8
João Pedro Maciel <joaopedromaciel007@gmail.com>
2025-02-13 23:50:41 -0300

    style: remove unused param

--- src/components/pricing.tsx
+++ src/components/pricing.tsx
@@ -55,7 +55,7 @@ export default function Pricing() {
       <div className="container mx-auto">
         <h2 className="text-3xl font-bold text-center mb-12">Escolha o Plano Ideal para Você</h2>
         <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
-          {plans.map((plan, index) => (
+          {plans.map((plan) => (
             <div key={plan.name} className="border p-8 rounded-lg shadow-md flex flex-col">
               {plan.moreChoosed && <Badge className="flex self-end bg-cyan-300">Mais escolhido</Badge>}
               <h3 className="text-2xl font-bold mb-4">{plan.name}</h3>
